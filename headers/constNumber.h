6f395d15962d6332d680844163761cfc553eb714
Tindur20 <sigsteinn@gmail.com>
2019-10-08 09:41:11 +0000

    cleaned up repo

--- headers/constNumber.h
+++
@@ -1,5 +0,0 @@
-#define BASETIME 1050
-#define BASEDIST 53
-#define TURNDIST 248
-#define TURNGIRO 720
-//#define BASEDIST 0.1
--- verkefni3/Head/constNumber.h
+++
@@ -1,6 +0,0 @@
-#define BASETIME 1050
-#define BASEDIST 53
-#define TURNDIST 248
-#define TURNGIRO 720
-#include "motorFunction.c"
-//#define BASEDIST 0.1
